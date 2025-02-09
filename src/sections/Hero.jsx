bb1bc4ec43b8966055a7a3e38365b7f3626d4018
Azhar Ali Sheeno <azharalisheeno009@gmail.com>
2025-02-09 20:40:38 +0500

    Work Done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -18,9 +18,9 @@ const Hero = () => {
         className="relative flex flex-col justify-center items-start pt-16 w-full max-xl:padding-x xl:w-2/5"
       >
         <p className="text-xl font-montserrat text-coral-red">Our Summer collections</p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="relative z-10 pr-10 xl:bg-white xl:whitespace-nowrap">The New Arrival</span>
-          <br />
+        <h1 className="mt-10 text-justify font-palanquin text-8xl max-sm:text-[39px] max-sm:leading-[39px] font-bold">
+          <span className="relative z-10 md:pr-10 xl:bg-white xl:whitespace-nowrap">The New Arrival</span>
+    
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-justify text-slate-gray text-lg mt-6 mb-14 sm:max-w-sm">
